122073f3bf94cf47b09303fe0cb1e91657db0e8c
Konstantin <you@example.com>
2021-04-01 01:25:27 +0300

    Fix Б16

--- src/store/data/selectors.js
+++ src/store/data/selectors.js
@@ -6,10 +6,7 @@ import {getOffersByLocation} from "../../utils/place";
 import {VISIBLE_OFFERS_NEARBY_COUNT, VISIBLE_REVIEW_COUNT} from "../../const";
 
 export const getOffers = (state) => state[NameSpace.DATA].offers;
-export const getOfferListLoadedStatus = (state) => state[NameSpace.DATA].isOfferListLoaded;
-export const getOffer = (state) => state[NameSpace.DATA].offer;
 export const getOfferID = (state) => state[NameSpace.DATA].offer.id;
-export const getOfferLoadingStatus = (state) => state[NameSpace.DATA].isOfferLoading;
 export const getOffersNearby = (state) => state[NameSpace.DATA].offersNearby;
 
 export const getComments = createSelector(
